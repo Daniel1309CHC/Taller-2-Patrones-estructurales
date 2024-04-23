5bc35ced89e39d4135162950a8d34e100a2da65a
Daniel Chaparro Cano <danielkano5@gmail.com>
2024-04-22 22:00:37 -0500

    Fix facade name

--- src/facade/app/OrderManager.java
+++ src/facade/app/OrderManager.java
@@ -2,55 +2,55 @@ package facade.app;
 
 // Fachada para el sistema de gestión de pedidos
 public class OrderManager {
-    private Product productSubsystem;
-    private Customer customerSubsystem;
-    private ShoppingCart shoppingCartSubsystem;
-    private OrderProcess orderProcessingSubsystem;
+    private Product product;
+    private Customer customer;
+    private ShoppingCart shoppingCar;
+    private OrderProcess orderProcess;
 
     public OrderManager() {
-        this.productSubsystem = new Product();
-        this.customerSubsystem = new Customer();
-        this.shoppingCartSubsystem = new ShoppingCart();
-        this.orderProcessingSubsystem = new OrderProcess();
+        this.product = new Product();
+        this.customer = new Customer();
+        this.shoppingCar = new ShoppingCart();
+        this.orderProcess = new OrderProcess();
     }
 
     // Métodos para gestionar productos
     public void addProductToInventory(String productName, double price, int quantity) {
-        productSubsystem.addProductToInventory(productName, price, quantity);
+        product.addProductToInventory(productName, price, quantity);
     }
 
     public void removeProductFromInventory(String productName, int quantity) {
-        productSubsystem.removeProductFromInventory(productName, quantity);
+        product.removeProductFromInventory(productName, quantity);
     }
 
     // Métodos para gestionar clientes
     public void registerCustomer(String name, String email, String address) {
-        customerSubsystem.registerCustomer(name, email, address);
+        customer.registerCustomer(name, email, address);
     }
 
     public void updateCustomerAddress(String email, String newAddress) {
-        customerSubsystem.updateCustomerAddress(email, newAddress);
+        customer.updateCustomerAddress(email, newAddress);
     }
 
     // Métodos para manejar el carrito de compra
     public void addToCart(String productName, int quantity) {
-        shoppingCartSubsystem.addToCart(productName, quantity);
+        shoppingCar.addToCart(productName, quantity);
     }
 
     public void removeFromCart(String productName, int quantity) {
-        shoppingCartSubsystem.removeFromCart(productName, quantity);
+        shoppingCar.removeFromCart(productName, quantity);
     }
 
     public void viewCart() {
-        shoppingCartSubsystem.viewCart();
+        shoppingCar.viewCart();
     }
 
     // Métodos para procesar pedidos
     public void processOrder(String orderId) {
-        orderProcessingSubsystem.processOrder(orderId);
+        orderProcess.processOrder(orderId);
     }
 
     public void cancelOrder(String orderId) {
-        orderProcessingSubsystem.cancelOrder(orderId);
+        orderProcess.cancelOrder(orderId);
     }
 }
